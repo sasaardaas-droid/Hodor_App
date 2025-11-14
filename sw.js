0c014c63dd7159eb610747c62e4b7bae9572622c
sasaardaas-droid <sasaardaas@gmail.com>
2025-11-14 18:59:30 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+// Service Worker pour Hodor
+const CACHE_NAME = 'hodor-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE).catch(err => {
+        return cache.add('./index.html');
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (!response || response.status !== 200) return response;
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      })
+      .catch(error => {
+        return caches.match(event.request)
+          .then(response => response || caches.match('./index.html'))
+          .catch(() => {
+            return new Response('Offline', {status: 503});
+          });
+      })
+  );
+});
